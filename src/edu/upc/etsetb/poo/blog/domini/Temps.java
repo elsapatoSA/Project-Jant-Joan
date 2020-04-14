39c422e98831a71598aa51ba94ae9a3cd294f3a0
gabri <gabri@LAPTOP-62IBIE85>
2020-04-14 20:45:46 +0200

    Temps casi acabat (falta toString i comparaTemps)

--- src/edu/upc/etsetb/poo/blog/domini/Temps.java
+++ src/edu/upc/etsetb/poo/blog/domini/Temps.java
@@ -5,6 +5,8 @@
  */
 package edu.upc.etsetb.poo.blog.domini;
 
+import java.time.LocalDateTime;
+
 public class Temps {
     String dia, hora;
 
@@ -14,9 +16,28 @@ public class Temps {
         this.hora="";                
     }
     public Temps(String temps){
-        String constr[];
-        constr = temps.split(" ");  
-        this.dia=constr[0];
-        this.hora=constr[1];
+        String separat[];
+        separat = temps.split(" ");  
+        this.dia=separat[0];
+        this.hora=separat[1];
+    }
+    
+    //Altres mètodes:
+    public void ara(){
+        String temps=LocalDateTime.now().toString(),separat[];
+        separat=temps.split("T");
+        this.dia=separat[0];
+        this.hora=separat[1];
+    }
+    public int comparaDia(Temps altre){
+        String parts[]=altre.dia.split("-"), parts_this[]=this.dia.split("-");
+        int any=Integer.parseInt(parts[0]), mes=Integer.parseInt(parts[1]), dia=Integer.parseInt(parts[0]);
+        int any_this=Integer.parseInt(parts_this[0]), mes_this=Integer.parseInt(parts_this[1]), dia_this=Integer.parseInt(parts_this[0]);
+        if(any_this>any || any_this==any && mes_this>mes || any_this==any && mes_this==mes && dia_this>dia)
+            return(1);
+        else if (any_this<any || any_this==any && mes_this<mes || any_this==any && mes_this==mes && dia_this<dia)
+            return(-1);
+        else
+            return(0);
     }
 }
